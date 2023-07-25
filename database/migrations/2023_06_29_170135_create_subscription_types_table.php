0f2e908898eb716cb089c051811f6777c498b809
Rostick0 <87297459+Rostick0@users.noreply.github.com>
2023-07-25 14:13:11 +0300

    +add page pricing

--- database/migrations/2023_06_29_170135_create_subscription_types_table.php
+++ database/migrations/2023_06_29_170135_create_subscription_types_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->text('description')->nullable();
+            $table->text('advantages')->nullable();
             $table->float('price');
             $table->timestamps();
         });
--- resources/views/pages_db/pricing.blade.php
+++ resources/views/pages_db/pricing.blade.php
@@ -1,7 +1,36 @@
 @extends('layout.front.index')
 
 @section('php')
+    @php
+        $subscription_types = App\Models\SubscriptionType::all();
+    @endphp
 @endsection
 
 @section('html')
+    <section class="pricing">
+        <div class="container">
+            <div class="pricing__container">
+                <h1 class="section-title-big pricing__title">Pricing plan</h1>
+                <ul class="pricing__list">
+                    @foreach ($subscription_types as $subscription_type)
+                        <li class="pricing__item pricing-item">
+                            <div class="pricing-item__block">
+                                <div class="pricing-item__title">{{ $subscription_type->name }}</div>
+                                <div class="pricing-item__info">{{ $subscription_type->price }},
+                                    {{ $subscription_type->description ?? '' }}</div>
+                                <button class="button-gradient pricing-item__button">Buy</button>
+                            </div>
+                            @if ($subscription_type->advantages)
+                                <div class="pricing-item__advantages">{{ $subscription_type->advantages }}</div>
+                            @endif
+                        </li>
+                    @endforeach
+                </ul>
+                <div class="pricing__buttons">
+                    <a class="button-gradient pricing__button" href="/login">Sing in</a>
+                    <a class="button-white pricing__button" href="/register">Sing up</a>
+                </div>
+            </div>
+        </div>
+    </section>
 @endsection
