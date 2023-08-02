11b94bd63eec83ca6904ab2ff88b32f8757313cf
Rostick0 <87297459+Rostick0@users.noreply.github.com>
2023-08-02 14:01:04 +0300

    +add belong music_kit

--- app/Models/MusicKit.php
+++ app/Models/MusicKit.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class MusicKit extends Model
 {
@@ -16,4 +17,9 @@ class MusicKit extends Model
         'duration',
         'music_id'
     ];
+
+    public function music(): BelongsTo
+    {
+        return $this->belongsTo(Music::class);
+    }
 }
