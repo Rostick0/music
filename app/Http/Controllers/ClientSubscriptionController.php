f5d059496fa832c4901fe8082d6d3cc7f302a223
Rostick0 <87297459+Rostick0@users.noreply.github.com>
2023-08-06 12:03:09 +0300

    +add client subscriptions

--- app/Http/Controllers/ClientSubscriptionController.php
+++ app/Http/Controllers/ClientSubscriptionController.php
@@ -7,29 +7,32 @@ use Illuminate\Http\Request;
 
 class ClientSubscriptionController extends Controller
 {
-    public function index(Request $request) {
+    public function index(Request $request)
+    {
         $subscription_types = Subscription::all();
 
         $where_sql = [];
-        if ($request->email)  $where_sql[] = ['users.email', 'LIKE', '%' . $request->email . '%'];
+        // if ($request->email)  $where_sql[] = ['users.email', 'LIKE', '%' . $request->email . '%'];
+        $where_sql[] = ['users.id', '=' , 2];
 
         $subscriptions = Subscription::select(
             'subscriptions.*',
             'subscription_types.name as subscription_name',
             // 'subscription_types.price as subscription_price',
-            'users.email as user_email'
+            'users.email as user_email',
         )
             ->join('subscription_types', 'subscription_types.id', '=', 'subscriptions.subscription_types_id')
             ->join('users', 'users.id', '=', 'subscriptions.user_id')
             ->orderByDesc('subscriptions.id')
             ->where($where_sql);
 
-        if ($request->subscription_types) {
-            $subscriptions->whereIn('subscriptions.subscription_types_id', $request->subscription_types);
-        }
+        if ($request->subscription_types) $subscriptions->whereIn('subscriptions.subscription_types_id', $request->subscription_types);
 
-        $subscriptions = $subscriptions->paginate(app('site')->count_admin ?? 20);
+        $subscriptions = $subscriptions->paginate(app('site')->count_admin);
 
-        return view('client.subscription_list');
+        return view('client.subscription_list', [
+            'subscriptions' => $subscriptions,
+            'subscription_types' => $subscription_types
+        ]);
     }
 }
