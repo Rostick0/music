6dde93221f3eabe4fab37b0472a90c06414e13d8
Rostick0 <87297459+Rostick0@users.noreply.github.com>
2023-08-20 12:06:56 +0300

    !fix scroll

--- resources/js/app.js
+++ resources/js/app.js
@@ -80,30 +80,18 @@ import { asyncSelect } from './ui';
 (function () {
     const links = document.querySelectorAll('a[href*="#"]');
 
-    if (links?.length) return;
+    if (!links?.length) return;
 
     links?.forEach(function (item) {
         item.addEventListener('click', function (e) {
             e.preventDefault();
 
-            // для каждого якоря берем соответствующий ему элемент и определяем его координату Y
-            let coordY = document.querySelector(item.getAttribute('href')).getBoundingClientRect().top + window.pageYOffset;
+            const coordY = document.querySelector(item.getAttribute('href')).getBoundingClientRect().top;
 
-            // запускаем интервал, в котором
-            let scroller = setInterval(function () {
-                // считаем на сколько скроллить за 1 такт
-                let scrollBy = coordY / framesCount;
-
-                // если к-во пикселей для скролла за 1 такт больше расстояния до элемента и дно страницы не достигнуто
-                if (scrollBy > window.pageYOffset - coordY && window.innerHeight + window.pageYOffset < document.body.offsetHeight) {
-                    // то скроллим на к-во пикселей, которое соответствует одному такту
-                    window.scrollBy(0, scrollBy);
-                } else {
-                    // иначе добираемся до элемента и выходим из интервала
-                    window.scrollTo(0, coordY);
-                    clearInterval(scroller);
-                }
-            }, 20 / 300);
+            window.scroll({
+                top: coordY,
+                behavior: 'smooth'
+              });
         });
     });
 })();
